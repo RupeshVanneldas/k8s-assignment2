76fb96ead71cb13e7bb9e553a89d7c49415e410c
RupeshVanneldas <rvanneldas2000@gmail.com>
2025-03-09 03:07:03 -0400

    shell changes

--- assignments.sh
+++ assignments.sh
@@ -4,10 +4,16 @@
 # Note: This script is tested on Windows 11 with WSL2 and Amazon Linux 2 EC2 instance. For run this script you need to have kind and kubectl installed on your machine (Hint: You can check init_kind.sh script to install kind and kubectl on your machine).
 
 # Clone the repository and navigate to the directory
+git clone https://github.com/RupeshVanneldas/k8s-assignment2.git
 
 # Create a kind cluster with the provided kind.yaml file for that do the following:
+cd k8s-assignment2
 chmod +x ./init_kind.sh
 ./init_kind.sh
+
+# Add /usr/local/bin to PATH if not already added
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+source ~/.bashrc
 
 # Check the version of kind and kubectl
 kind version
@@ -45,7 +51,9 @@ kubectl apply -f web/service.yaml
 kubectl get pods --all-namespaces # You should see both web and mysql pod running
 kubectl get svc --all-namespaces # You should see both web and mysql service running
 
-# To access the web pod, you can port-forward the web pod to your local machine:
+# Wait for some time
+
+# To access the web pod, you can port-forward the web pod to your local machine (Completely optional): 
 kubectl port-forward -n web-ns pod/web-pod 8080:8080
 # Do http://localhost:8080/ in a browser to see the web page or "curl http://localhost:8080/" in a seperate terminal
 
@@ -57,7 +65,7 @@ kubectl apply -f mysql/replicaset.yaml
 kubectl apply -f web/replicaset.yaml
 # Verify by:
 kubectl get rs -n mysql-ns
-kubectl get rs -n web-ns
+kubectl get rs -n web-ns    
 # To scale the replicas of the web pod (Completely optional):
 kubectl scale --replicas=6 rs/web-rs -n web-ns
 kubectl get rs -n web-ns
